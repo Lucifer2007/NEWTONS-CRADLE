6ba3d5239999191006847db5e2abe14e1cdee4eb
Lucifer2007 <75356485+Lucifer2007@users.noreply.github.com>
2021-08-20 21:16:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,67 +3,42 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
 
 
 function setup() {
-	createCanvas(1300, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paper1=new Paper();
 
-	//Create the Bodies Here.
-roof = new Roof(650, 350, 400 , 30)
-bob1 = new Bob(340, 500 , 40)
-bob2 = new Bob(420, 500 , 40)
-bob3 = new Bob(500, 500 , 40)
-bob4 = new Bob(580, 500 , 40)
-bob5 = new Bob(660, 500 , 40)
-rope1 = new Rope(roof.body, bob1.body, -160, 0)
-rope2 = new Rope(roof.body, bob2.body, -80,0)
-rope3 = new Rope(roof.body, bob3.body,0,0)
-rope4 = new Rope(roof.body, bob4.body,80,0)
-rope5 = new Rope(roof.body, bob5.body,160,0)
+
 	Engine.run(engine);
   
-
 }
 
 
 function draw() {
-background("lightblue");
-  
+  rectMode(CENTER);
+  background("black");
+ 
 
-
-
- rope1.display();
- rope2.display();
- rope3.display();
- rope4.display();
- rope5.display();
- roof.display();
- bob1.display();
- bob2.display();
- bob3.display();
- bob4.display();
- bob5.display();
+  groundObject.display();
+  dustbinObj.display();
+  paper1.display();
 
 }
-function keyPressed (){
-if (keyCode===UP_ARROW){
-Body.applyForce(bob1.body,bob1.body.position, {x:-150 , y: -150} )
-
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:50,y:-100})
+	}
 }
-
-
-if (keyCode ===DOWN_ARROW){
-	Body.applyForce(bob5.body,bob5.body.position, {x:150 , y: -150} )
-}
-}
-
-
-
-
-
 
